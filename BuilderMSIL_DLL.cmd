06b2d7e23d6bc9c89d06d5eff71e51abcf3455b8
ITVDN-com <78604391+ITVDN-com@users.noreply.github.com>
2021-02-28 20:15:37 +0200

    Publication

---
+++ BuilderMSIL_DLL.cmd
@@ -0,0 +1,4 @@
+C:\Windows\Microsoft.NET\Framework\v2.0.50727\ilasm /dll D:\StaticMethodInInterface\StaticMethodInInterface.il
+
+
+pause
---
+++ StaticMethodInInterfaceSample/StaticMethodInInterfaceSample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+
+using DLLibrary.StaticMethodInInterface;
+
+namespace StaticMethodInInterfaceSample
+{
+    class MyClass : IInterface
+    {
+        // Статичесикй метод интерфейса не наследуется.
+        // Система автодополнения предлагает реализовать интерфейс.        
+        //public void Method()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+    class Program
+    {
+        static void Main()
+        {
+            // Напрямую метод - Method - недоступен!
+            // IInterface.Method(); 
+            // MyClass.Method();    
+            
+            // Использование рефлексии.
+
+            Type type = typeof(IInterface);
+
+            //MethodInfo method = type.GetMethod("Method", BindingFlags.Static); // Ошибка!
+            MethodInfo method = type.GetMethod("Method");
+            method.Invoke(null, null);
+
+            // Статичесикй метод интерфейса не наследуется.
+            type = typeof(MyClass);
+
+            method = type.GetMethod("Method");
+
+            if (method != null)
+                method.Invoke(null, null);
+            else
+                Console.WriteLine("Метод с именем Method в классе MyClass не найден!");
+
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
